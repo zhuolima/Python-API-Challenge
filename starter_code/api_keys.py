734414db7e008c31b703f615ae06528f62ddee99
zhuolima <zhuolima@outlook.com>
2021-11-28 10:59:37 -0700

    Add files via upload

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "8866e48767ca50c8d5677dc308e2bc02"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDjecWpNB5n3ql5TmB8SgdgJa42HkCe_VI"
